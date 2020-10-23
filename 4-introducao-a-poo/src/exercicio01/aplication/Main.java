52b77670b181eebca373004afd879d0bfc9d6e5b
vaz_mf <vazfernandam@gmail.com>
2020-10-23 17:16:10 -0300

    Resolução do Ex01

--- 4-introducao-a-poo/src/exercicio01/aplication/Main.java
+++ 4-introducao-a-poo/src/exercicio01/aplication/Main.java
@@ -25,5 +25,6 @@ public class Main {
         System.out.printf("AREA: %.2f\n", rectangle.area());
         System.out.printf("PERIMETER: %.2f\n", rectangle.perimeter());
         System.out.printf("DIAGONAL: %.2f\n", rectangle.diagonal());
+        input.close();
     }
 }
