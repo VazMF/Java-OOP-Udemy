9b3f86aee490b159a7bc33bdcab1ecf394ea5caa
vaz_mf <vazfernandam@gmail.com>
2021-01-19 14:56:03 -0300

    Exemplo de Herança

---
+++ 9-heranca-polimorfismo/src/application/Main.java
@@ -0,0 +1,18 @@
+package application;
+
+import entities.Account;
+import entities.BusinessAccount;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        Account account = new Account();
+        BusinessAccount businessAccount = new BusinessAccount();
+
+
+        input.close();
+    }
+}
---
+++ 9-heranca-polimorfismo/src/entities/BusinessAccount.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class BusinessAccount extends Account{
+
+    private Double loanLimit;
+
+    public BusinessAccount() {
+        super();
+    }
+
+    public BusinessAccount(Integer _number, String _holder, Double _balance, Double _loanLimit) {
+        super(_number, _holder, _balance);
+        loanLimit = _loanLimit;
+    }
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double _loanLimit) {
+        loanLimit = _loanLimit;
+    }
+
+    public void loan(double amount) {
+       if (amount <= loanLimit) {
+           balance += amount = 10.0;
+       }
+    }
+}
