9dbe1104b1ec7ce0efeee146f0ff79a92ad3cc8b
vaz_mf <vazfernandam@gmail.com>
2021-01-27 15:39:04 -0300

    Upcasting e Downcasting

--- 9-heranca-polimorfismo/src/aula125/application/Main.java
+++ 9-heranca-polimorfismo/src/aula125/application/Main.java
@@ -7,8 +7,34 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Account account = new Account();
-        BusinessAccount businessAccount = new BusinessAccount();
+        Account account = new Account(1001, "Alex", 0.0);
+        BusinessAccount businessAccount = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+        // Upcasting
+
+        Account account1 = businessAccount;
+        Account account2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+        Account account3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+
+        // Downcasting
+
+        BusinessAccount account4 = (BusinessAccount)account2;
+        account4.loan(100.0);
+
+        // BusinessAccount account5 = (BusinessAccount)account3;
+
+        if (account3 instanceof BusinessAccount) {
+            BusinessAccount account5 = (BusinessAccount)account3;
+            account5.loan(200.0);
+            System.out.println("Loan!");
+        }
+
+        if (account3 instanceof SavingsAccount) {
+            SavingsAccount account5 = (SavingsAccount)account3;
+            account5.updateBalance();
+            System.out.println("Update!");
+        }
+
 
         input.close();
     }
---
+++ 9-heranca-polimorfismo/src/aula125/entities/SavingsAccount.java
@@ -0,0 +1,26 @@
+package aula125.entities;
+
+public class SavingsAccount extends Account{
+
+    private Double interestRate;
+
+    public SavingsAccount(){
+    }
+
+    public SavingsAccount(Integer _number, String _holder, Double _balance, Double _interestRate) {
+        super(_number, _holder, _balance);
+        interestRate = _interestRate;
+    }
+
+    public Double getInterestRate(){
+        return interestRate;
+    }
+
+    public void setInterestRate(Double _interestRate){
+        interestRate = _interestRate;
+    }
+
+    public void updateBalance() {
+        balance += balance * interestRate;
+    }
+}
