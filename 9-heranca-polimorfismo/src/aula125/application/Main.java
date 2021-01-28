94718e2b5d51c5e25498ee249a9eb7db4484c00e
vaz_mf <vazfernandam@gmail.com>
2021-01-28 14:14:53 -0300

    introdução ao polimorfismo

--- 9-heranca-polimorfismo/src/aula125/application/Main.java
+++ 9-heranca-polimorfismo/src/aula125/application/Main.java
@@ -1,46 +1,17 @@
 package aula125.application;
 
 import aula125.entities.*;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        Account account = new Account(1001, "Alex", 0.0);
-        BusinessAccount businessAccount = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+        Account x = new Account(1020, "Alex", 1000.0);
+        Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
 
-        // Upcasting
+        x.withdraw(50.0);
+        y.withdraw(50.0);
 
-        Account account1 = businessAccount;
-        Account account2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
-        Account account3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
-
-        // Downcasting
-
-        BusinessAccount account4 = (BusinessAccount)account2;
-        account4.loan(100.0);
-
-        // BusinessAccount account5 = (BusinessAccount)account3;
-
-        if (account3 instanceof BusinessAccount) {
-            BusinessAccount account5 = (BusinessAccount)account3;
-            account5.loan(200.0);
-            System.out.println("Loan!");
-        }
-
-        if (account3 instanceof SavingsAccount) {
-            SavingsAccount account5 = (SavingsAccount)account3;
-            account5.updateBalance();
-            System.out.println("Update!");
-        }
-
-        // Sobreposição
-
-        Account account5 = new BusinessAccount(1005, "Bob", 1000.0, 500.0);
-        account5.withdraw(200.0);
-        System.out.println(account5.getBalance());
-
-        input.close();
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
     }
 }
