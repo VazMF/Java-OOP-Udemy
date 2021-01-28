dea5ee0c3c6e0c63d325811313d0689c1490b26e
vaz_mf <vazfernandam@gmail.com>
2021-01-28 14:05:18 -0300

    implementando classes e métodos final

--- 9-heranca-polimorfismo/src/aula125/entities/SavingsAccount.java
+++ 9-heranca-polimorfismo/src/aula125/entities/SavingsAccount.java
@@ -1,6 +1,6 @@
 package aula125.entities;
 
-public class SavingsAccount extends Account{
+public final class SavingsAccount extends Account{
 
     private Double interestRate;
 
@@ -25,7 +25,7 @@ public class SavingsAccount extends Account{
     }
 
     @Override
-    public void withdraw(double amount) {
+    public final void withdraw(double amount) {
         balance -= amount;
     }
 }
